f22b09b350c84f37eb9fa5287daa963bb2803603
xarksyuz <randas.edso@gmail.com>
2025-08-12 04:02:19 +0700

    update templates in settings, create urls.py in cars app and create index html in templates

--- carShowRoom/carShowRoom/settings.py
+++ carShowRoom/carShowRoom/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'carShowRoom.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
