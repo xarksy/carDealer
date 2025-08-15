85b33c3a0cfff5ca8c981f665219893887ab7d59
xarksyuz <randas.edso@gmail.com>
2025-08-16 04:10:46 +0700

    adding the views to urls

--- carShowRoom/cars/urls.py
+++ carShowRoom/cars/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import carList
+from .views import carList, create_car
 
 urlpatterns = [
     path('',carList,name='carList'),
+    path('create/',create_car,name='create_car'),
 ]
