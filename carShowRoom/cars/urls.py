d793fcd3eb3aec9545a10b41a12aaa18b1689108
xarksyuz <randas.edso@gmail.com>
2025-08-17 05:01:30 +0700

    menambahkan detail car di urls

--- carShowRoom/cars/urls.py
+++ carShowRoom/cars/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import carList, create_car
+from .views import carList, create_car, detail_car
 
 urlpatterns = [
     path('',carList,name='carList'),
     path('create/',create_car,name='create_car'),
+    path('detail/<int:car_id>', detail_car, name='detail_car'),
 ]
