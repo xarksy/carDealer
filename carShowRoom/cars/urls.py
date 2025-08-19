6caea2740f7a3b94eb403f7e0d08b8945cbd8e8e
xarksyuz <randas.edso@gmail.com>
2025-08-20 04:15:30 +0700

    add update views to urls

--- carShowRoom/cars/urls.py
+++ carShowRoom/cars/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import carList, create_car, detail_car
+from .views import carList, create_car, detail_car, updateCar
 
 urlpatterns = [
     path('',carList,name='carList'),
     path('create/',create_car,name='create_car'),
     path('detail/<int:car_id>', detail_car, name='detail_car'),
-    path('update')
+    path('update/<int:car_id>', updateCar, name='update_car'),
 ]
