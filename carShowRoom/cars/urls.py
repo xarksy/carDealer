6d05f7524e32f0099369adf21e1aabafae12b118
xarksyuz <randas.edso@gmail.com>
2025-08-21 04:17:39 +0700

    menambahkan def car service ke urls

--- carShowRoom/cars/urls.py
+++ carShowRoom/cars/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import carList, create_car, detail_car, updateCar, deleteCar
+from .views import carList, create_car, detail_car, updateCar, deleteCar, car_service_plain
 
 urlpatterns = [
     path('',carList,name='carList'),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('detail/<int:car_id>', detail_car, name='detail_car'),
     path('update/<int:car_id>', updateCar, name='update_car'),
     path('delete/<int:car_id>', deleteCar, name='delete_car'),
+    path('service/',car_service_plain,name='car_service'),
 ]
