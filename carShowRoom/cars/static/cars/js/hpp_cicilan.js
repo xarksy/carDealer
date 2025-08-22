7621e1ae79c4b83de43ede664ec32f19ec92bc94
xarksyuz <randas.edso@gmail.com>
2025-08-23 05:18:48 +0700

    cari elemen interest

--- carShowRoom/cars/static/cars/js/hpp_cicilan.js
+++ carShowRoom/cars/static/cars/js/hpp_cicilan.js
@@ -1,5 +1,7 @@
 window.addEventListener('DOMContentLoaded', function () {
     const cashInput = document.getElementById('cash');
     const tenorInput = document.getElementById('tenor');
+    const interestInput = document.getElementById('interest');
 
+    
 })
