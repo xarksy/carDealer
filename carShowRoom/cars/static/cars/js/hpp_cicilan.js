0d6f588f3133df4d6ab637f057192fcc25380aad
xarksyuz <randas.edso@gmail.com>
2025-08-24 14:17:08 +0700

    adding perhitungan cicilan pembelan

--- carShowRoom/cars/static/cars/js/hpp_cicilan.js
+++ carShowRoom/cars/static/cars/js/hpp_cicilan.js
@@ -7,5 +7,29 @@ window.addEventListener('DOMContentLoaded', function () {
     const resetButton = document.getElementById('reset_hitungan');
 
     const angsuranBulanan = document.getElementById('angsuran_bulanan');
+    const totalPembayaran = document.getElementById('total_pembayaran');
+    const totalBunga = document.getElementById('total_bunga');
+
+    calculateButton.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        const cash = parseFloat(cashInput.value.replace(/,/g,'')) || 0;
+        const tenor = parseInt(tenorInput.value) || 0;
+        const interest = parseFloat(interestInput.value) || 0;
+
+        if (cash && tenor && interest) {
+            const monthlyInterestRate = interest / 100 / 12;
+            const monthlyPayment = (cash * monthlyInterestRate) / (1 - Math.pow(1 + monthlyInterestRate, -tenor));
+            const totalPayment = monthlyPayment * tenor;
+            const totalInterest = totalPayment - cash;
+
+            angsuranBulanan.innerText = `Rp${monthlyPayment.toLocaleString()}`;
+            totalPembayaran.innerText = `Rp${totalPayment.toLocaleString()}`;
+            totalBunga.innerText = `Rp${totalInterest.toLocaleString()}`;
+
+
+        }
+    });
+
     
 })
