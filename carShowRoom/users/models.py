39720d39c9d1e57338ee9fc21e09837496968b51
xarksyuz <randas.edso@gmail.com>
2025-08-27 13:13:46 +0700

    add user model and remigrate the db

--- carShowRoom/users/models.py
+++ carShowRoom/users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+    # Add custom user role field
+    ROLE_CHOICES = (
+        ("admin", "Admin"),
+        ("salesperson", "Salesperson"),
+        ("customer", "Customer"),
+    )
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="customer")
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
