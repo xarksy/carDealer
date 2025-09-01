b3a3517715c4d6b6c74c6fab88706504d64758ca
xarksyuz <randas.edso@gmail.com>
2025-09-01 16:50:15 +0700

    feat(customer): adding customer form

--- carShowRoom/customer/models.py
+++ carShowRoom/customer/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+    CUSTOMER_TYPE_CHOICES = [
+        ("seller", "Seller"),
+        ("trade_in", "Trade-In"),
+        ("buyer", "Buyer"),
+    ]
+
+    STATUS_CHOICES = [
+        ("deal","Deal"),
+        ("negotiating","Negotiating"),
+        ("not_deal", "Not Deal"),
+        ("uncontacted", "Uncontacted"),
+        ("rna","RNA (Return, No Answer)"),
+    ]
+
+    name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20, unique=True)
+    email = models.EmailField(blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+
+    customer_type = models.CharField(max_length=20, choices=CUSTOMER_TYPE_CHOICES)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="uncontacted")
+    notes = models.TextField(blank=True, null=True)
+
+    # info tambahan
+    car_condition = models.CharField(max_length=50, blank=True, null=True)
+    offer_price = models.DecimalField(max_digits=15, decimal_places=2, blank=True, null=True)
+
+    budget = models.DecimalField(max_digits=15, decimal_places=2, blank=True, null=True)
+
+    # tracking waktu
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.customer_type}, {self.status})"
