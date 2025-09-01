354267fe3c73a990be6673e0365e662223d639af
xarksyuz <randas.edso@gmail.com>
2025-09-01 13:20:08 +0700

    feat(users): display userlist in superuser dashboard

--- carShowRoom/users/urls.py
+++ carShowRoom/users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import userView
+from .views import userView, register_view, userlist_view
 
 urlpatterns = [
     path('', userView, name='user_view'),
+    path('userlist/', userlist_view, name='user_list'),
+    path('register/', register_view, name='register'),
 ]
--- carShowRoom/users/views.py
+++ carShowRoom/users/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+from .models import User
 
 # Create your views here.
 def userView(request):
 
     return render(request,'base.html')
+
+def register_view(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('user_list')
+    else:
+        form = CustomUserCreationForm()
+    
+    return render(request, 'users/register.html', {'form':form})
+
+def userlist_view(request):
+
+    context = {
+        'users' : User.objects.all()
+    }
+
+    return render(request,'users/user_index.html',context)
+
