1ab1c153f6d82aa17ff9b224b224fc925da9fe3f
xarksyuz <randas.edso@gmail.com>
2025-09-03 20:01:04 +0700

    feat(orders): categorizing order types

--- carShowRoom/orders/models.py
+++ carShowRoom/orders/models.py
@@ -3,3 +3,11 @@ from customer.models import Customer
 from cars.models import Car
 
 # Create your models here.
+class Order(models.Model):
+    ORDER_TYPE_CHOICES = [
+        ("sell","Sell"),
+        ("trade","Trade"),
+        ("buy","Buy"),
+    ]
+
+    
