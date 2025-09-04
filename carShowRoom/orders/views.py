fd8dbdc5674497ef7027b0683fbaa95f769c0976
xarksyuz <randas.edso@gmail.com>
2025-09-05 05:14:40 +0700

    feat(order): adding trade view

--- carShowRoom/orders/views.py
+++ carShowRoom/orders/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Order
 from customer.models import Customer
 from .forms import OrderForm
@@ -7,14 +7,19 @@ from customer.forms import CustomerForm
 # Create your views here.
 def placing_order_view(request):
     action = request.GET.get("action",None)
+    order_form, customer_form = None, None
 
-    if action == 'A':
-        context = {
-            'teksnya' : 'Halo A',
-        }
-    elif action == 'B':
-        context = {
-            'teksnya' : 'halo B',
-        }
-    
+    if request.method == "POST":
+        if action == "Trade":
+            order_form = OrderForm(request.POST)
+            customer_form = CustomerForm(request.POST)
+            if order_form.is_valid() and customer_form.is_valid():
+                customer = customer_form.save()
+                order = order_form.save(commit=False)
+                order.customer = customer
+                order.offer_type = "trade"
+                order.save()
+                return redirect('detail')
+                
+
     return render(request,'orders/order_form.html', context)
