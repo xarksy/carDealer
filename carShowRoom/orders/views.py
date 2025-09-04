f4158ae14a35578f59348f7b947d5005f71599ff
xarksyuz <randas.edso@gmail.com>
2025-09-05 04:46:05 +0700

    importing form

--- carShowRoom/orders/views.py
+++ carShowRoom/orders/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Order
 from customer.models import Customer
+from .forms import OrderForm
+from customer.forms import CustomerForm
 
 # Create your views here.
 def placing_order_view(request):
