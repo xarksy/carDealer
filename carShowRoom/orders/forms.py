47af1b5a35f3553d2490c698be1288f09a6a85a2
xarksyuz <randas.edso@gmail.com>
2025-09-04 17:18:56 +0700

    feat(order): creating orderform

---
+++ carShowRoom/orders/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from models import Order
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = [
+            "offered_vehicle_model",
+            "offered_vehicle_varian",
+            "offered_vehicle_transmission",
+            "offered_vehicle_province",
+            "offered_vehicle_year",
+            "offered_vehicle_kilometers",
+            "offered_vehicle_notes",
+        ]
