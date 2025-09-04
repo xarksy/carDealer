cfdf497542cb50a0c3ea54d2d856f90da274f92b
xarksyuz <randas.edso@gmail.com>
2025-09-04 20:11:46 +0700

    feat(chore): adding orders url to main

--- carShowRoom/carShowRoom/urls.py
+++ carShowRoom/carShowRoom/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login/',login_view, name='login'),
     path('logout/',logout_view,name='logout'),
     path('users/',include('users.urls')),
+    path('orders/',include('orders.urls')),
 ]
