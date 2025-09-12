1fc33f05537cc65ebdf784fa8a15b6f543779373
xarksyuz <randas.edso@gmail.com>
2025-09-13 06:50:50 +0700

    feat(cars-js): adding event listener to openbtn click

--- carShowRoom/cars/static/cars/js/custom.js
+++ carShowRoom/cars/static/cars/js/custom.js
@@ -21,6 +21,9 @@ const openBtn = document.getElementById("openBtn");
 const closeBtn = document.getElementById("closeBtn");
 const popup = document.getElementById("popup");
 
+openBtn.addEventListener("click", () => {
+  popup.style.display = "flex";
+});
 
 function attachListeners() {        
     document.querySelectorAll('.close').forEach(function(button) {
