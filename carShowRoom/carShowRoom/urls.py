ec674831fef34fa551f6fedb1c8a8e55678923cf
xarksyuz <randas.edso@gmail.com>
2025-10-01 04:05:28 +0700

    feat(core-urls): adding api cars to the route

--- carShowRoom/carShowRoom/urls.py
+++ carShowRoom/carShowRoom/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from .views import login_view, logout_view
+from rest_framework.routers import DefaultRouter
+from cars.api import CarsViewSet
+
+router = DefaultRouter()
+router.register(r'cars', CarsViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,4 +14,5 @@ urlpatterns = [
     path('logout/',logout_view,name='logout'),
     path('users/',include('users.urls')),
     path('orders/',include('orders.urls')),
+    path('api',include(router.urls)),
 ]
