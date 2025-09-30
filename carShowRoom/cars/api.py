3474a777f278e4a18e4f688df218b9c6ee816806
xarksyuz <randas.edso@gmail.com>
2025-10-01 04:01:09 +0700

    feat(cars-api): creating cars serializers

---
+++ carShowRoom/cars/api.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers, viewsets
+from .models import Cars
+
+class CarsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cars
+        fields = "__all__"
