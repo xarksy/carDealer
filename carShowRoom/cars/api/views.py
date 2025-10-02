a29848e2da33896658abc0d07ed5ec805addb0f0
xarksyuz <randas.edso@gmail.com>
2025-10-03 04:04:13 +0700

    feat(cars-api): importing permissions into api view

--- carShowRoom/cars/api/views.py
+++ carShowRoom/cars/api/views.py
@@ -1,7 +1,9 @@
 from rest_framework import  viewsets
 from models import Cars
 from serializers import CarsSerializer
+from .permissions import IsAdminSuperuser
 
 class CarsViewSet(viewsets.ModelViewSet):
     queryset = Cars.objects.all()
     serializer_class = CarsSerializer
+    permission_classes = [IsAdminSuperuser]
