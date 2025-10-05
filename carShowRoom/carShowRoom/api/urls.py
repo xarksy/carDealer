032c58455bd44a0a40aadc64edac76af1c48a019
xarksyuz <randas.edso@gmail.com>
2025-10-05 13:09:44 +0700

    feat(core-api): registering token refresh to api urls

--- carShowRoom/carShowRoom/api/urls.py
+++ carShowRoom/carShowRoom/api/urls.py
@@ -3,4 +3,5 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('token/',TokenObtainPairView.as_view(), name='token_obtain_view'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh_view'),
 ]
