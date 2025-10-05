13614bd3a80b287bfa7084ab1c9f8871337ad619
xarksyuz <randas.edso@gmail.com>
2025-10-06 06:39:29 +0700

    feat(core-permissions): adding staff or read only permissions

--- carShowRoom/carShowRoom/api/permissions.py
+++ carShowRoom/carShowRoom/api/permissions.py
@@ -8,3 +8,10 @@ class IsAdminOrSuperuser(BasePermission):
             and user.is_authenticated
             and (user.is_authenticated or getattr(user, "role", None) == "admin")
         )
+
+class IsStaffOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in ['GET','HEAD']:
+            return True
+        return request.user.is_staff
+
