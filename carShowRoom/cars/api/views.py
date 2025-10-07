6c4d0ad0996c8cb853cfe7fb1560b10c5fbfdbee
xarksyuz <randas.edso@gmail.com>
2025-10-07 18:19:36 +0700

    feat(cars-api): adding filters to automatically get data from urls page

--- carShowRoom/cars/api/views.py
+++ carShowRoom/cars/api/views.py
@@ -1,5 +1,5 @@
 from rest_framework.permissions import IsAuthenticated
-from rest_framework import  viewsets
+from rest_framework import  viewsets, filters
 from ..models import Cars
 from .serializers import CarsSerializer
 from carShowRoom.api.permissions import IsAdminOrSuperuser
@@ -8,4 +8,7 @@ from carShowRoom.api.permissions import IsAdminOrSuperuser
 class CarsViewSet(viewsets.ModelViewSet):
     queryset = Cars.objects.all()
     serializer_class = CarsSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['nama','merek']
+    ordering_fields = ['harga', 'tahun']
     permission_classes = [IsAdminOrSuperuser]
