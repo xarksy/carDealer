2246f68e031db6051c48695143c92692869f8e46
xarksyuz <randas.edso@gmail.com>
2025-10-12 00:36:26 +0700

    feat(orders-api): importing necessary files into order serializers

---
+++ carShowRoom/orders/api/serializers.py
@@ -0,0 +1,5 @@
+from rest_framework import serializers
+from models import Order, TradeInCar
+from customer.api.serializers import CustomerSerializer
+from cars.api.serializers import CarsSerializer
+
