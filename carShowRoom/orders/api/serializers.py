944184770611a45afe3663ec43165179cabe2e84
xarksyuz <randas.edso@gmail.com>
2025-10-12 00:39:45 +0700

    feat(order-api): adding tradein serializers

--- carShowRoom/orders/api/serializers.py
+++ carShowRoom/orders/api/serializers.py
@@ -3,3 +3,13 @@ from models import Order, TradeInCar
 from customer.api.serializers import CustomerSerializer
 from cars.api.serializers import CarsSerializer
 
+class TradeInCarSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TradeInCar
+        fields = [
+            'id', 'offered_vehicle_model', 'offered_vehicle_varian',
+            'offered_vehicle_transmission', 'offered_vehicle_province',
+            'offered_vehicle_year', 'offered_vehicle_kilometers',
+            'offered_vehicle_notes'
+        ]
+
