cb9868029c6bcc485bce12926c2d34fed26711d2
xarksyuz <randas.edso@gmail.com>
2025-10-12 00:44:44 +0700

    feat(order-api): adding orderserializer

--- carShowRoom/orders/api/serializers.py
+++ carShowRoom/orders/api/serializers.py
@@ -13,3 +13,26 @@ class TradeInCarSerializer(serializers.ModelSerializer):
             'offered_vehicle_notes'
         ]
 
+class OrderSerializer(serializers.ModelSerializer):
+    customer = CustomerSerializer(read_only=True)
+    showroom_car = CarsSerializer(read_only=True)
+    trade_in_car = TradeInCarSerializer(read_only=True)
+
+    offer_type_display = serializers.CharField(source='get_offer_type_display', read_only=True)
+    created_at_formatted = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Order
+        fields = [
+            'id',
+            'customer',
+            'showroom_car',
+            'offer_type',
+            'offer_type_display',
+            'trade_in_car',
+            'created_at',
+            'created_at_formatted'
+        ]
+    
+    def get_created_at_formatted(self, obj):
+        return obj.created_at.strftime("%d %b %Y, %H:%M")
