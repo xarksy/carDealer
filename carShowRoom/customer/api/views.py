6b8bd8c0bc8c95ad661eaf894d0f32ad8d2be22c
xarksyuz <randas.edso@gmail.com>
2025-10-13 06:11:51 +0700

    feat(customer-api): adding destroy function to customer api views

--- carShowRoom/customer/api/views.py
+++ carShowRoom/customer/api/views.py
@@ -43,3 +43,11 @@ class CustomerViewSet(viewsets.ModelViewSet):
             {"message": "Customer successfully updated", "data": serializer.data},
             status=status.HTTP_200_OK
         )
+    
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(
+            {"message": "Customer successfully deleted"},
+            status=status.HTTP_204_NO_CONTENT
+        )
