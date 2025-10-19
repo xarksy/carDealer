aa9eba851e537f61489b8b3023074733b8a01e3d
xarksyuz <randas.edso@gmail.com>
2025-10-20 05:21:15 +0700

    feat(core-settings): adding corsheaders to settings

--- carShowRoom/carShowRoom/settings.py
+++ carShowRoom/carShowRoom/settings.py
@@ -43,16 +43,18 @@ INSTALLED_APPS = [
     'customer',
     'orders',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',    
 ]
 
 ROOT_URLCONF = 'carShowRoom.urls'
@@ -147,4 +149,11 @@ REST_FRAMEWORK = {
         'rest_framework.filters.OrderingFilter',
     ]
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
