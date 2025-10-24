d85b2df8aa603f876f3165cc93ab99b642fc64c2
xarksyuz <randas.edso@gmail.com>
2025-10-24 10:19:55 +0700

    fix(customer-model): fixing user field

--- carShowRoom/customer/models.py
+++ carShowRoom/customer/models.py
@@ -11,7 +11,7 @@ class Customer(models.Model):
         ("rna","RNA (Return, No Answer)"),
     ]
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="customer_profile")
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="customer_profile",null=True,blank=True)
     name = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=20, unique=True)
     email = models.EmailField(blank=True, null=True)
