2c0857afb3138790c7522d9d0c36465e283d8bb3
xarksyuz <randas.edso@gmail.com>
2025-10-24 10:16:48 +0700

    feat(customer-model): importing get user mudel so that it can use to query in api viewset

--- carShowRoom/customer/models.py
+++ carShowRoom/customer/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 class Customer(models.Model):
     STATUS_CHOICES = [
@@ -9,6 +11,7 @@ class Customer(models.Model):
         ("rna","RNA (Return, No Answer)"),
     ]
 
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="customer_profile")
     name = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=20, unique=True)
     email = models.EmailField(blank=True, null=True)
