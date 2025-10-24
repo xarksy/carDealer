5fcde4f87bb67237ff83818c362e96da14a0c3b0
xarksyuz <randas.edso@gmail.com>
2025-10-24 12:44:35 +0700

    feat(demo-urls): routing the views in urls

---
+++ carShowRoom/demo/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from views import demo_api_dashboard, demo_api_login, demo_api_logout
+
+urlpatterns = [
+    path('login/', demo_api_login, name='demo_api_login'),
+    path('dashboard/', demo_api_dashboard, name='demo_api_dashboard'),
+    path('logout/', demo_api_logout, name='demo_api_logout'),
+]
