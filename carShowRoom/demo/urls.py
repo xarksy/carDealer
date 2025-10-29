b5ce832e63f9e79d8d358597e08aa38a61b1c319
xarksyuz <randas.edso@gmail.com>
2025-10-30 04:59:59 +0700

    feat(demo-urls): adding userlist to route

--- carShowRoom/demo/urls.py
+++ carShowRoom/demo/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import dashboard_of_dashboard, demo_login, demo_logout, demo_carlist_dashboard
+from users.views import userlist_view
 
 urlpatterns = [
     path('login/', demo_login, name='demo_login'),
     path('dashboard/', dashboard_of_dashboard, name='demo_dashboard'),
     path('dashboard/carlist/', demo_carlist_dashboard, name='demo_carlist_dashboard'),
+    path('dashboard/userlist/', userlist_view, name='demo_userlist_dashboard'),
     path('logout/', demo_logout, name='demo_logout'),
 ]
