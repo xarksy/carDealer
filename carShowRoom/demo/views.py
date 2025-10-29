87531a9faddeada94a17c8770b2b4a5516869ecd
xarksyuz <randas.edso@gmail.com>
2025-10-30 04:48:14 +0700

    feat(demo-views): adding demo dashboard chart to views

--- carShowRoom/demo/views.py
+++ carShowRoom/demo/views.py
@@ -53,6 +53,32 @@ def demo_api_dashboard(request):
     cars = Cars.objects.all()
     return render(request, "demo/base1.html", {"cars": cars})
 
+# ========================= > Dashboard
+
+@admin_required
+def dashboard_of_dashboard(request):    
+    total_order = Order.objects.count()
+    total_customers = Customer.objects.count()
+    total_cars = Cars.objects.count()
+
+    total_income = Order.objects.aggregate(total=Sum("showroom_car__harga"))["total"] or 0
+    order_per_month = (
+        Order.objects.values("created_at__month")
+        .annotate(total=Count("id"))
+        .order_by("created_at__month")
+    )
+
+    context = {
+        'total_order' : total_order,
+        'total_customer' : total_customers,
+        'total_cars' : total_cars,
+        'total_income' : total_income,
+        'order_per_month' : order_per_month,
+    }
+    
+
+    return render(request, 'demo/demo_dashboard.html', context=context)
+
 def demo_carlist_dashboard(request):
     if not request.user.is_authenticated:
         return redirect("demo_login")
