4ed88d670fdafdbdec3378be86e4088b3c3f1fae
xarksyuz <randas.edso@gmail.com>
2025-10-30 05:04:53 +0700

    feat(demo-urls): adding customer list dashboard to route

--- carShowRoom/demo/urls.py
+++ carShowRoom/demo/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import dashboard_of_dashboard, demo_login, demo_logout, demo_carlist_dashboard
+from .views import dashboard_of_dashboard, demo_login, demo_logout, demo_carlist_dashboard, dashboard_customer_list
 from users.views import userlist_view
 
 urlpatterns = [
     path('login/', demo_login, name='demo_login'),
     path('dashboard/', dashboard_of_dashboard, name='demo_dashboard'),
-    path('dashboard/carlist/', demo_carlist_dashboard, name='demo_carlist_dashboard'),
     path('dashboard/userlist/', userlist_view, name='demo_userlist_dashboard'),
+    path('dashboard/carlist/', demo_carlist_dashboard, name='demo_carlist_dashboard'),
+    path('dashboard/customerlist/', dashboard_customer_list, name='demo_customer_list_dashboard'),
     path('logout/', demo_logout, name='demo_logout'),
 ]
