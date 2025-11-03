0bb544b54571560a5ed21dc34bb2943097f8a8d9
xarksyuz <randas.edso@gmail.com>
2025-11-04 04:53:18 +0700

    feat(user-api): registering to urls

--- carShowRoom/users/api/urls.py
+++ carShowRoom/users/api/urls.py
@@ -1,7 +1,8 @@
-# from rest_framework.routers import DefaultRouter
-# from .views import UserProfileView
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet
 
-# router = DefaultRouter()
-# router.register(r'user/profile/', UserProfileView.as_view())
+router = DefaultRouter()
+router.register('users', UserViewSet)
 
-# urlpatterns = router.urls
+
+urlpatterns = router.urls
