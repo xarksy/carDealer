7923d8725dd300135137861e6c867e4900d39c29
xarksyuz <randas.edso@gmail.com>
2025-11-04 04:46:04 +0700

    feat(core-api): routing user profile to apis urls

--- carShowRoom/carShowRoom/api/urls.py
+++ carShowRoom/carShowRoom/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import CustomTokenObtainPairView
+from .views import CustomTokenObtainPairView, UserProfileView
 
 urlpatterns = [
     path('token/',CustomTokenObtainPairView.as_view(), name='token_obtain_view'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh_view'),
+    path('user/profile/', UserProfileView.as_view(), name='user-profile'),
     path('', include('cars.api.urls')),
     path('', include('orders.api.urls')),
     path('', include('customer.api.urls')),
