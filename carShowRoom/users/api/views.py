cc15f9f49f41dbb5834fc4468fa84c71f601f68f
xarksyuz <randas.edso@gmail.com>
2025-11-04 04:51:44 +0700

    feat(users-api): adding UserViewSet to users api view

--- carShowRoom/users/api/views.py
+++ carShowRoom/users/api/views.py
@@ -1,14 +1,12 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from users.api.serializers import UserSerializer
 from .serializers import UserSerializer
 from rest_framework.permissions import IsAuthenticated
 from carShowRoom.api.permissions import IsAdminOrSuperuser, IsStaffOrReadOnly
+from rest_framework import  viewsets, filters, permissions, status
+from ..models import User
 
 
-class UserProfileView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request):
-        serializer = UserSerializer(request.user)
-        return Response(serializer.data)
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAdminOrSuperuser]
