09d9cd1da47cb3c1277c7e1f4cf1e35b718b408b
xarksyuz <randas.edso@gmail.com>
2025-11-05 04:30:32 +0700

    feat(core-settings): adding pagination to settings

--- carShowRoom/carShowRoom/settings.py
+++ carShowRoom/carShowRoom/settings.py
@@ -156,7 +156,9 @@ REST_FRAMEWORK = {
         'django_filters.rest_framework.DjangoFilterBackend',
         'rest_framework.filters.SearchFilter',
         'rest_framework.filters.OrderingFilter',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
 
 CORS_ALLOWED_ORIGINS = [
