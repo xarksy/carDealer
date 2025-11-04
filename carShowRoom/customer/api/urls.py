e88a8c2ea5fb8d584fda1855eed9cca67983d55f
xarksyuz <randas.edso@gmail.com>
2025-11-05 04:38:20 +0700

    feat(customer-api-urls): registering customer register view to urls

--- carShowRoom/customer/api/urls.py
+++ carShowRoom/customer/api/urls.py
@@ -1,7 +1,11 @@
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CustomerViewSet
+from .views import CustomerViewSet, CustomerRegisterView
 
 router = DefaultRouter()
-router.register(r'customers', CustomerViewSet)
+router.register(r'customers', CustomerViewSet, basename='customer')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('', include(router.urls)),
+    path('register/',CustomerRegisterView.as_view(),name='customer-register'),
+]
