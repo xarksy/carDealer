475a9c0770f59ebad216fd25db5d991acadbd485
xarksyuz <randas.edso@gmail.com>
2025-11-06 04:40:06 +0700

    feat(cars-api): adding custom update car custom message in views

--- carShowRoom/cars/api/views.py
+++ carShowRoom/cars/api/views.py
@@ -23,6 +23,17 @@ class CarsViewSet(viewsets.ModelViewSet):
             "data": response.data
         }, status=status.HTTP_201_CREATED)
 
+    def update(self, request, *args, **kwargs):
+        """Handle both PUT and PATCH for updating cars."""
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(
+            {"message": "Car successfully updated", "data": serializer.data},
+            status=status.HTTP_200_OK
+        )
 
     def get_permissions(self):
         user = self.request.user
