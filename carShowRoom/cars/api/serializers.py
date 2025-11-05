0097d063871b8402fa7d9c25fbf46695e12c0f01
xarksyuz <randas.edso@gmail.com>
2025-11-06 04:50:15 +0700

    feat(cars-api): adding servicehistory serializers

--- carShowRoom/cars/api/serializers.py
+++ carShowRoom/cars/api/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from ..models import Cars
+from ..models import Cars, ServiceHistory
+
+class ServiceHistorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ServiceHistory
+        fields = ['id','tanggal_servis','deskripsi','biaya']
 
 class CarsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cars
         fields = "__all__"
+
