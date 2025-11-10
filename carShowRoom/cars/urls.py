5c76b88e12844cd19d57ad4e66855a5d05a2dd92
xarksyuz <randas.edso@gmail.com>
2025-11-10 21:45:37 +0700

    fix(cars-urls): removing dashboard cars urls

--- carShowRoom/cars/urls.py
+++ carShowRoom/cars/urls.py
@@ -11,9 +11,5 @@ urlpatterns = [
     path('delete/<int:car_id>/', deleteCar, name='delete_car'),
     path('service/',car_service_plain,name='car_service'),
     path('service/<int:car_id>/', car_service, name='service_history'),
-    path('dashboard/',dashboard_of_dashboard, name='dashboard'),
-    path('dashboard/user_list/', userlist_view, name='dashboard_user_list'),
-    path('dashboard/car_list/',dashboard_car_list, name='dashboard_car_list'),
-    path('dashboard/customer_list/',dashboard_customer_list, name='dashboard_customer_list'),
     path('success/',success_page, name='success_page'),
 ]
