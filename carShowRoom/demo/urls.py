ddfe5becdfbe6db87e455c51599624ebf6af5d69
xarksyuz <randas.edso@gmail.com>
2025-11-19 06:20:30 +0700

    feat(demo-urls): routing demo api views

--- carShowRoom/demo/urls.py
+++ carShowRoom/demo/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import dashboard_of_dashboard, demo_login, demo_logout, demo_carlist_dashboard, dashboard_customer_list
+from .views import dashboard_of_dashboard, demo_login, demo_logout, demo_carlist_dashboard, dashboard_customer_list, demo_api
 from users.views import userlist_view
 
 urlpatterns = [
@@ -8,5 +8,6 @@ urlpatterns = [
     path('dashboard/userlist/', userlist_view, name='demo_userlist_dashboard'),
     path('dashboard/carlist/', demo_carlist_dashboard, name='demo_carlist_dashboard'),
     path('dashboard/customerlist/', dashboard_customer_list, name='demo_customer_list_dashboard'),
+    path('api/', demo_api, name='demo_api'),
     path('logout/', demo_logout, name='demo_logout'),
 ]
