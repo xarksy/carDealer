85e60b386ae04967a0916f8c6e6cb4f0e6e57cc9
xarksyuz <randas.edso@gmail.com>
2025-12-24 07:48:24 +0700

    feat(cars-admin): add carimageinline

--- carShowRoom/cars/admin.py
+++ carShowRoom/cars/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import Cars, ServiceHistory
+from .models import Cars, ServiceHistory, CarImage
 
 # Register your models here.
 admin.site.register(Cars)
 admin.site.register(ServiceHistory)
+
+
+# 1. Tampilan inline untuk gambar
+class CarImageInline(admin.TabularInline):
+    model = CarImage
+    extra = 3 # Menampilkan 3 slot upload kosong secara default
