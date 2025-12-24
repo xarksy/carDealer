c3b88d9d9936d1da81f9bada24b42a02358859bf
xarksyuz <randas.edso@gmail.com>
2025-12-24 07:51:02 +0700

    feat(cars-admin): add car admin to upload many images

--- carShowRoom/cars/admin.py
+++ carShowRoom/cars/admin.py
@@ -1,12 +1,21 @@
 from django.contrib import admin
 from .models import Cars, ServiceHistory, CarImage
 
+# 1. Tampilan inline untuk gambar
+class CarImageInline(admin.TabularInline):
+    model = CarImage
+    extra = 3 # Menampilkan 3 slot upload kosong secara default
+
+# 2. Masukkan Inline tersebut ke dalam Admin Mobil
+class CarAdmin(admin.ModelAdmin):
+    # Menambahkan fitur upload banyak gambar di halaman edit mobil
+    inlines = [CarImageInline]
+
+    list_display = ('nama','merek','harga','status')
+
+
 # Register your models here.
 admin.site.register(Cars)
 admin.site.register(ServiceHistory)
 
 
-# 1. Tampilan inline untuk gambar
-class CarImageInline(admin.TabularInline):
-    model = CarImage
-    extra = 3 # Menampilkan 3 slot upload kosong secara default
