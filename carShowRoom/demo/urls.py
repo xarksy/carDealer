3519586724f9d26db2a2f809e25fc5d5df93d02c
xarksyuz <randas.edso@gmail.com>
2026-01-24 16:38:20 +0700

    feat(demo-urls): add to urls customer update status

--- carShowRoom/demo/urls.py
+++ carShowRoom/demo/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import dashboard_of_dashboard, demo_login, demo_logout, demo_carlist_dashboard, dashboard_customer_list, demo_api
+from .views import dashboard_of_dashboard, demo_login, demo_logout, demo_carlist_dashboard, dashboard_customer_list, demo_api, update_customer_status
 from users.views import userlist_view
 from activity_log.views import dashboard_log
 
@@ -7,9 +7,10 @@ urlpatterns = [
     path('login/', demo_login, name='demo_login'),
     path('dashboard/', dashboard_of_dashboard, name='demo_dashboard'),
     path('dashboard/userlist/', userlist_view, name='demo_userlist_dashboard'),
-        path('dashboard/logs/', dashboard_log, name='dashboard_log_list'),
+    path('dashboard/logs/', dashboard_log, name='dashboard_log_list'),
     path('dashboard/carlist/', demo_carlist_dashboard, name='demo_carlist_dashboard'),
     path('dashboard/customerlist/', dashboard_customer_list, name='demo_customer_list_dashboard'),
+    path('dashboard/customer/status/<int:customer_id>/', update_customer_status, name='update_customer_status'),
     path('api/', demo_api, name='demo_api'),
     path('logout/', demo_logout, name='demo_logout'),
 ]
