207effc0f6210e7f048abfeec1d318da4602930b
xarksyuz <randas.edso@gmail.com>
2026-01-30 21:00:58 +0700

    fix(demo-css): remove comments

--- carShowRoom/carShowRoom/settings.py
+++ carShowRoom/carShowRoom/settings.py
@@ -132,8 +132,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]  # optional: if you keep custom static files inside /static/
 
 # 👇 Add this line — this is where Django will collect static files to
-# STATIC_ROOT = BASE_DIR / "staticfiles"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR / "staticfiles"
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
